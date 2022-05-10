1bad98c131d9baf973510f0a310f3ae2fc4bd644
martinavujovska <95173501+martinavujovska@users.noreply.github.com>
2022-05-10 17:08:52 +0200

    Add files via upload

---
+++ Queue/Queue/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> numbers = new Queue<int>();
+            while (true)
+            {
+                Console.Write("Please enter a number: ");
+                if (!int.TryParse(Console.ReadLine(), out int num))
+                {
+                    Console.WriteLine("Wrong input, please enter a number");
+                    continue;
+                }
+                numbers.Enqueue(num);
+                Console.WriteLine("Would you like to enter a different number? (Yes/No) ");
+                string input = Console.ReadLine();
+                if (input.ToLower() != "yes")
+                {
+                    break;
+                }
+                continue;
+            }
+            while (numbers.TryDequeue(out int num))
+            {
+                Console.WriteLine(num);
+            }
+
+        }
+    }
+}
